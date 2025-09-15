b459dfc38c7cc80cbe5e8d2d1405ca89e184d273
tomerdu <tomerduchovni@gmail.com>
2025-09-15 16:21:21 +0300

    add import data with random birthday for now

---
+++ import_data.php
@@ -0,0 +1,42 @@
+<?php
+require 'Database.php';
+
+$db = new Database();
+
+function fetchJson($url) {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($response, true);
+}
+
+// USERS
+
+$users = fetchJson("https://jsonplaceholder.typicode.com/users");
+
+foreach ($users as $user) {
+    $birthdate = date("Y-m-d", strtotime(rand(1970, 2000) . "-" . rand(1, 12) . "-" . rand(1, 28)));
+
+    $db->insert(
+        "INSERT INTO users (id, name, email, active, birthdate) VALUES (?, ?, ?, ?, ?)",
+        [$user['id'], $user['name'], $user['email'], 1, $birthdate]
+    );
+}
+
+echo "Users imported successfully!<br>";
+
+// POSTS 
+
+$posts = fetchJson("https://jsonplaceholder.typicode.com/posts");
+
+foreach ($posts as $post) {
+    $created_at = date("Y-m-d H:i:s", strtotime("-" . rand(0, 30) . " days"));
+
+    $db->insert(
+        "INSERT INTO posts (id, user_id, title, body, created_at, active) VALUES (?, ?, ?, ?, ?, ?)",
+        [$post['id'], $post['userId'], $post['title'], $post['body'], $created_at, 1]
+    );
+}
+
+echo "Posts imported successfully!";
